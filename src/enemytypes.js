9165b9ba426e6e1a339a86aedf3c6d970e825662
jani-nykanen <jnyknn@gmail.com>
2020-10-27 21:19:40 +0200

    More enemies

--- src/enemytypes.js
+++ src/enemytypes.js
@@ -18,7 +18,8 @@ export function getEnemyType(index) {
 		Caterpillar, SandEgg, Fly, 
 		Bat, Fish, Star, 
 		Snowman, Apple, Rock,
-		Plant, Block
+		Plant, Block, ManEater,
+		Spook
 	];
     
     return TYPES[clamp(index, 0, TYPES.length-1) | 0];
@@ -1349,4 +1350,163 @@ export class Block extends Enemy {
 		}
 	}
 
+}
+
+
+export class ManEater extends Enemy {
+	
+	
+	constructor(x, y) {
+		
+		super(x, y, 14, 5, 3);
+
+		this.center.y = 2;
+		this.collisionBox = new Vector2(4, 10);
+        // this.hitbox = new Vector2(8, 8);
+		this.renderOffset.y = 0;
+        
+        this.friction.x = 0.025;
+		this.friction.y = 0.1;
+
+		this.mass = 0.5;
+
+        this.dir = 1 - 2 * (((x / 16) | 0) % 2);
+
+		this.bounceX = -1;
+		this.bounceY = -1;
+	}
+
+
+	init(x, y) {
+
+		const BASE_GRAVITY = 4.0;
+		const BASE_SPEED = 0.5;
+
+		this.dir = 1 - 2 * (((x / 16) | 0) % 2);
+
+		this.target.x = this.dir * BASE_SPEED;
+		this.speed.x = this.target.x;
+
+		this.target.y = BASE_GRAVITY;
+	}
+
+
+	updateAI(ev) {
+		
+        // ...
+	}
+	
+	
+	animate(ev) {
+		
+		const ANIM_SPEED = 6;
+
+		this.spr.animate(this.spr.row, 0, 3, ANIM_SPEED, ev.step);
+	
+		this.flip = this.dir < 0 ? Flip.None : Flip.Horizontal;
+	}
+
+
+    wallCollisionEvent(x, y, h, dir, ev) {
+
+		this.dir = -dir;
+		this.speed.x *= -1;
+		this.target.x = this.speed.x;
+	}
+
+	
+	floorCollisionEvent(x, y, w, ev) {
+
+		const JUMP_HEIGHT = -2.25;
+
+		this.speed.y = JUMP_HEIGHT;
+
+        // Sound effect
+        ev.audio.playSample(ev.assets.samples["jump2"], 0.50);
+	}
+
+
+	enemyCollisionEvent(e) {
+
+		if ((this.speed.x > 0 && e.pos.x > this.pos.x) ||
+			(this.speed.x < 0 && e.pos.x < this.pos.x )) {
+
+			this.target.x *= -1;
+			this.speed.x *= -1;
+
+			this.dir *= -1;
+		}
+	}
+
+}
+
+
+export class Spook extends Enemy {
+	
+	
+	constructor(x, y) {
+		
+		super(x, y, 15, 3, 2);
+		
+		this.friction.x = 0.015;
+		this.friction.y = 0.033;
+		
+		this.collisionBox = new Vector2(6, 6);
+        // this.hitbox = new Vector2(8, 8);
+
+		this.mass = 0.5;
+
+		this.moveDir = new Vector2(0, 0);
+
+		this.waveTimer = 0;
+
+		this.disableCollisions = true;
+	}
+	
+	
+	init(x, y) {
+
+		this.waveTimer = 0;
+	}
+	
+	
+	updateAI(ev) {
+
+		const MOVE_SPEED = 0.33;
+		const WAVE_SPEED = 0.05;
+		const AMPLITUDE = 1.5;
+
+		this.waveTimer = (this.waveTimer + WAVE_SPEED * ev.step) % (Math.PI*2);
+
+		this.target.x = this.moveDir.x * MOVE_SPEED;
+		this.target.y = (this.moveDir.y + AMPLITUDE * Math.sin(this.waveTimer)) 
+						* MOVE_SPEED;
+		
+	}
+	
+	
+	animate(ev) {
+		
+		const EPS = 0.25;
+
+		let frame = 1;
+		if (this.speed.y < -EPS)
+			frame = 0;
+		else if (this.speed.y > EPS)
+			frame = 2;
+
+		this.spr.setFrame(frame, this.spr.row);
+
+		this.flip = this.target.x > 0 ? Flip.Horizontal : Flip.None;
+	}
+
+
+	playerEvent(pl, ev) {
+
+		this.moveDir = (
+			new Vector2(pl.pos.x - this.pos.x, 
+				pl.pos.y - this.pos.y))
+			.normalize(true);
+		
+	}
 }
